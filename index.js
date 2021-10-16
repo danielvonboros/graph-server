35ba33f6ad15fb8bbfa0fec0958261266eb852a5
danielvonboros <dboros40@gmail.com>
2021-10-16 16:48:15 +0200

    add config for CORS policy

--- index.js
+++ index.js
@@ -2,18 +2,37 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 
-const graphData2 = require("./data.json");
+const graphData = require("./data.json");
 
 const PORT = 3001;
 
-// import graphData from "./data.js";
-
-app.use(cors());
 app.use(express.json());
+
+let allowedOrigins = [
+  "http://localhost:3000",
+  "https://localhost:3000",
+  "http://graph-server-mtg.heroku.com",
+  "https://graph-server-mtg.heroku.com",
+];
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        let message =
+          "The CORS policy for this application does not allow access from origin " +
+          origin;
+        return callback(new Error(message), false);
+      }
+      return callback(null, true);
+    },
+  })
+);
 
 app.get("/", (req, res) => {
   console.log("/ endpoint", req);
-  res.json(graphData2);
+  res.json(graphData);
 });
 
 app.get("/:graphId", (req, res) => {
